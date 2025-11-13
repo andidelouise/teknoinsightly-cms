e97d33210067b879f35e6d9577ba0f6ada81a97d
dior <94304320+andidelouise@users.noreply.github.com>
2025-11-13 13:30:55 +0700

    [Feat] : Add Login Auth and Manage Tutorial

---
+++ next.config.js
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+    return config;
+  },
+  redirects() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/dashboard/swiper",
+        permanent: false,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
